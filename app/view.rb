65ccd02db8f864698a3d6f5e2c17a3f44bef10f2
Konnor Rogers <konnor5456@gmail.com>
2022-12-10 09:29:13 -0500

    add hanami view

--- app/view.rb
+++ app/view.rb
@@ -4,5 +4,6 @@ require "slim"
 module Bookshelf
   class View < Hanami::View
     config.paths = [File.join(File.expand_path(__dir__), "templates")]
+    config.layout = "application"
   end
 end
--- app/views/home.rb
+++ app/views/home.rb
@@ -1,6 +1,7 @@
 module Views
   module Home
     class Show < Bookshelf::View
+      config.template = "home/show"
     end
   end
 end
